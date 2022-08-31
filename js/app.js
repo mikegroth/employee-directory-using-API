602edf84cba6fe713746dc4d34c7a471a546fe64
Mike Groth <mjaygroth@gmail.com>
2022-08-31 17:00:10 -0500

    update modal functionality, display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 let employees = [];
-let userSearch = [];
+let currentIndex = '';
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`
 const gridContainer = document.querySelector(".grid-container");
@@ -49,7 +49,6 @@ function displayEmployees(employeeInfo) {
 }
 
 function displayCard(index) {
-    console.log('displayCard function working');
     let name = employees[index].name;
     let email = employees[index].email;
     let picture = employees[index].picture;
@@ -61,8 +60,6 @@ function displayCard(index) {
     let postcode = employees[index].location.postcode;
     let dob = employees[index].dob.date;
     let newDOB = birthday(dob);
-    
-        
         
         let modalHTML = `
                     <button class="close-btn" id="close-card">X</button>
@@ -82,8 +79,13 @@ function displayCard(index) {
                     </div>`;
                     
             overlay.classList.remove("hidden");
-            Larrow.classList.remove("hidden");
-            Rarrow.classList.remove("hidden");
+
+            arrowUnhidden();
+            
+            if(searchedUserIndex.length !== 0) {
+                    searchArrowUnhidden();
+            }
+
             overlayCard.innerHTML = modalHTML;
 }
 
@@ -97,26 +99,21 @@ function birthday(date) {
     return outputDate;
 }
 
-//Search Function -- populate array of user names
-
-function getUsers() {
-    userSearch = employees.map(employee => {
-        let first = employee.name.first;
-        let last = employee.name.last;
-        return `${first} ${last}`;
-    }); return userSearch;
-}
 
 // Search function 
 
 const userSelect = document.getElementById('search-input');
 
+let searchedUserIndex = [];
+
 function searchUser() {
     let search = userSelect.value.toLowerCase();
     let html = '';
+    searchedUserIndex = [];
     for(let i = 0; i < employees.length; i++) {
         let nameCompare = `${employees[i].name.first} ${employees[i].name.last}`
         if (nameCompare.toLowerCase().includes(search)) {
+            searchedUserIndex.push(i);
             let employeeHTML = `
         <div class="card" id="employee-card-${i}" data-index="${i}">
             <img class="card-img" src="${employees[i].picture.large}" alt="employee photo placeholder">
@@ -141,6 +138,7 @@ gridContainer.addEventListener('click', (e) => {
     if (e.target.className !== 'grid-container') {
         const cardTarget = e.target.closest('.card');
         const index = cardTarget.getAttribute('data-index');
+        currentIndex = index;
         console.log(index);
         displayCard(index);
     }
@@ -150,25 +148,22 @@ overlayCard.addEventListener('click', (e) => {
             if(e.target.className === 'close-btn') {
                 console.log('clicking X Button');
                 overlay.classList.add('hidden');
+                Larrow.classList.add('hidden');
+                Rarrow.classList.add('hidden');
             }   
 });
-
-directHeader.addEventListener('click', (e) => {
-    e.target = searchbar;
-    if(userSearch.length === 0) {
-    getUsers(); 
-    }
-})
 
 const buttonL = document.getElementById('buttonL')
 const buttonR = document.getElementById('buttonR')
 
 buttonL.addEventListener('click', () => {
     console.log('clicking L Button');
+    previousCard()
 });
 
 buttonR.addEventListener('click', (e) => {
     console.log('clicking R Button');
+    nextCard();
 });
 
 userSelect.addEventListener('keyup', searchUser);
@@ -186,52 +181,76 @@ function removeBicycle() {
     bike.classList.remove('bicycle-wheelie');
 }
 
-function nextArrow(e) {
-    e.addEventListener('click', () => {
-        console.log(index);
-        nextCard(index);
-         
-    });
+
+function nextCard() {
+    if(searchedUserIndex.length === 0 || searchedUserIndex.length === employees.length) {
+        let nextIndex = (parseInt(currentIndex) + 1);
+        console.log(nextIndex);
+        displayCard(nextIndex);
+        currentIndex = nextIndex;
+        arrowUnhidden();
+        console.log(currentIndex);
+    } else {
+        let newIndex = searchedUserIndex.indexOf(parseInt(currentIndex));
+        console.log(newIndex);
+        let nextNewIndex = searchedUserIndex[newIndex + 1];
+        displayCard(nextNewIndex);
+        currentIndex = nextNewIndex;
+        searchArrowUnhidden();
+    }
 }
 
-function nextCard(index) {
-
-    console.log('displayCard function working');
-    console.log(employees[index]);
-    let name = employees[index].name;
-    let email = employees[index].email;
-    let picture = employees[index].picture;
-    let phone = employees[index].phone;
-    let city = employees[index].location.city;
-    let streetNumber = employees[index].location.street.number;
-    let street = employees[index].location.street.name;
-    let state = employees[index].location.state;
-    let postcode = employees[index].location.postcode;
-    let dob = employees[index].dob.date;
-    let newDOB = birthday(dob);
-    
-        
-        
-        let modalHTML = `
-                    <button class="close-btn" id="close-card">X</button>
-                    <div class="overlay-card-content">
-                        <img class="card-img overlay-item" src="${picture.large}" alt="">
-                        <div class="employee-info-box overlay-item">
-                            <h2 class="name">${name.first} ${name.last}</h2>
-                            <p class="email">${email}</p>
-                            <p class="address">${city}</p>
-                        </div>
-                    </div>
-
-                    <div class="employee-additional-info">
-                        <p>${phone}</p>
-                        <p class="address">${streetNumber} ${street}, ${state} ${postcode}</p>
-                        <p>Birthday: ${newDOB} </p>
-                    </div>`;
-
-            overlayCard.innerHTML = modalHTML;
+function previousCard() {
+    if(searchedUserIndex.length === 0 || searchedUserIndex.length === employees.length) {
+        let previousIndex = (parseInt(currentIndex) - 1);
+        console.log(previousIndex);
+        displayCard(previousIndex);
+        currentIndex = previousIndex;
+        arrowUnhidden();
+        console.log(currentIndex);
+    } else {
+        let newIndex = searchedUserIndex.indexOf(parseInt(currentIndex));
+        console.log(newIndex);
+        let previousNewIndex = searchedUserIndex[newIndex - 1];
+        displayCard(previousNewIndex);
+        currentIndex = previousNewIndex;
+        searchArrowUnhidden();
+    }
 }
 
+function arrowUnhidden() {
+    let indexValue = parseInt(currentIndex);
+    if(indexValue === 0) {
+        Rarrow.classList.remove("hidden");
+        Larrow.classList.add("hidden")
+    } else if (indexValue === (employees.length - 1)) {
+        Larrow.classList.remove("hidden");
+        Rarrow.classList.add("hidden")
+    } else {
+        Rarrow.classList.remove("hidden");
+        Larrow.classList.remove("hidden");
+    }
+}
+
+function searchArrowUnhidden() {
+    let searchedIndexValue = searchedUserIndex.indexOf(parseInt(currentIndex))
+    if(searchedIndexValue === 0) {
+        Rarrow.classList.remove("hidden");
+        Larrow.classList.add("hidden")
+    } else if (searchedIndexValue === (searchedUserIndex.length - 1)) {
+        Larrow.classList.remove("hidden");
+        Rarrow.classList.add("hidden")
+    } else {
+        Rarrow.classList.remove("hidden");
+        Larrow.classList.remove("hidden");
+    } 
+}
+
+
+//if the next index is included in the search, show it, if not, move on to the next card
+
+
+// really all we want is to display the card of the next index in the array 
 
 
 
